55fe89034a9420b5ea5e40b0631f80595c4549d7
Alexey <lesh77906@gmail.com>
2020-09-22 18:36:47 +0300

    tester console output

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ def main():
     connection = tarantool.connect("localhost", 3301)
     tester = connection.space('tester')
     tester1 = connection.space('tester1')
+
+    print("\033[33mtester\033[0m:")
+    print(tester.select(3))
+    print("\033[33mtester\033[31m1\033[0m:")
+    print(tester1.select(3))
+
+
+if __name__== "__main__":
+    main()
+
