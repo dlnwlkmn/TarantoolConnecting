a6e329f8b73aff6990d51a5d38a8063425424dc5
Alexey <lesh77906@gmail.com>
2020-09-23 14:31:27 +0300

    tester2 has been cleared by using "call" method (:truncate request)

--- main.py
+++ main.py
@@ -20,13 +20,17 @@ def main():
     print(tester)
     print(tester1)
 
-    print(f"ping: {connection.ping()}")
+    print(f"\nping: {connection.ping()}")
 
     tester2 = connection.space('tester2')
 
-    print("\033[33mtester\033[31m2\033[0m:")
+    print("\033[33m\ntester\033[31m2\033[0m:")
     print(tester2.select(), "\n")
 
+    connection.call('box.space.tester2:truncate', ())
+
+    print("\033[33m\ntester\033[31m2\033[0m:")
+    print(tester2.select(), "\n")
 
 if __name__== "__main__":
     main()
